89e4a2b948871103db89d793dfad55ec34adeeb7
J <jwr5361@gmail.com>
2022-12-27 13:13:48 -0700

    Update signal-server and begin chat client implementation.

---
+++ chat-client.js
@@ -0,0 +1,46 @@
+//create websocket for signaling
+let ws = new WebSocket("ws://localhost:8080", ['JSON']);
+
+//Turn server configuration for peer connection (open source)
+const config = {
+    iceServers: [
+      {
+        urls: "stun:relay.metered.ca:80",
+      },
+      {
+        urls: "turn:relay.metered.ca:80",
+        username: "your-turn-server-username",
+        credential: "your-turn-server-password",
+      },
+      {
+        urls: "turn:relay.metered.ca:443",
+        username: "your-turn-server-username",
+        credential: "your-turn-server-password",
+      },
+      {
+        urls: "turn:relay.metered.ca:443?transport=tcp",
+        username: "your-turn-server-username",
+        credential: "your-turn-server-password",
+      },
+    ],
+  };
+
+//create webRTC connection
+let pc = new RTCPeerConnection(config);
+
+//Websocket message event listener
+//Move functionality to its own module
+ws.onmessage(async (event) =>{
+    //Parse the message data from the signal server
+    let msg = JSON.parse(event.data)
+
+    //Handle any possible message from the signal server
+    switch(msg){
+        //Login
+        //Users
+        //Offer
+        //Answer
+        //New Ice Candidate
+    }
+})
+
